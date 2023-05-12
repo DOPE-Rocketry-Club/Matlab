d82f7134dad9c0648e865667a3d8a5853da82ad1
Samuele Lolli <samuele.lolli@gmail.com>
2023-05-12 14:31:31 +0200

    Update Codice Mathab

--- Analisi_dati_1.m
+++ Analisi_dati_1.m
@@ -1,46 +1,31 @@
-% Definire la deviazione standard dei dati casuali generati per accelerazione e angoli di Eulero
-accel_stddev = 0.1; % in m/s^2
-euler_stddev = 2; % in gradi
-
-filename = 'LOG_8.CVS';
+%File dati
+filename = 'IMU_0.CSV';
 filepath = 'C:\Users\samue\OneDrive - unige.it\Desktop\Matlab\data\Test-lavatrice\';
 
 % Caricare il file CSV come una matrice
-data = readtable("C:\Users\samue\OneDrive - unige.it\Desktop\Matlab\data\Test-lavatrice\IMU_0.CSV");
+data = readtable([filepath,filename]);
 dati_csv1 = data(:, 1:4); % time, x, y z
+
+%Dati con smooth
 var2 = smoothdata(dati_csv1.x,"loess",100)
 var3 = smoothdata(dati_csv1.y,"loess",100)
 var4 = smoothdata(dati_csv1.z,"loess",100)
 
 
-% Generare casualmente 100 dati di accelerazione (x, y, z)
-acceleration = [normrnd(0, accel_stddev, 100, 1),...
-                normrnd(0, accel_stddev, 100, 1),...
-                normrnd(9.81, accel_stddev, 100, 1)];
-
-% Generare casualmente 100 dati di angoli di Eulero (roll, pitch, yaw)
-euler_angles = [normrnd(0, euler_stddev, 100, 1),...
-                normrnd(0, euler_stddev, 100, 1),...
-                normrnd(0, euler_stddev, 100, 1)];
-
-z = linspace(0,4*pi,500);
-x = 2*cos(z) + rand(1,500);
-y = 2*sin(z) + rand(1,500);
-% Creare un grafico di dispersione 3D dell'accelerazione
-figure();
-%scatter3(x, y, z, 'filled');
+%Grafico 1
 hold on
 plot(dati_csv1.time, dati_csv1.x)
 plot(dati_csv1.time, dati_csv1.y)
 plot(dati_csv1.time, dati_csv1.z, "g")
+xlabel("Tempo");
+ylabel("X, Y, Z gradi");
 figure();
-scatter3(dati_csv1.x, dati_csv1.y, dati_csv1.z, '*');
-figure();
+
+
+%Grafico 2
 scatter3(var2, var3, var4, '*');
-hold off
-
-xlabel('X Acceleration (m/s^2)');
-ylabel('Y Acceleration (m/s^2)');
-zlabel('Z Acceleration (m/s^2)');
+xlabel('X Gradi');
+ylabel('Y Gradi');
+zlabel('Z Gradi');
 title('Accelerometer Data');
-
+hold off
